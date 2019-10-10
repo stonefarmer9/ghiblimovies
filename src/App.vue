49bc2d701b05c95ec2f457e1219ddef4db5e3647
Georges Le Vaillant <g.p.levaillant@gmail.com>
2019-10-10 15:22:52 +0100

    FEAT: movies display as styled cards & other styling

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <img alt="Vue logo" src="./assets/ghibli.png">
+    <FullMovieList v-if="movie === null"
+      :allMovies="allMovies"
+      @show:movie="showMovie"
+    />
+    <MovieCard v-else
+      :selectedMoviemovie="selectedMovie"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FullMovieList from '@/components/fullMovieList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    FullMovieList
+  },
+  data() {
+    return {
+      allMovies: [],
+      selectedMovie: null,
+    }
+  },
+  mounted(){
+    this.getMovies()
+  },
+  methods: {
+    async getMovies() {
+      try{
+        const response = await fetch('https://ghibliapi.herokuapp.com/films')
+        const data = await response.json()
+        this.allMovies = data
+        // eslint-disable-next-line
+        console.log("SUCCESS", data)
+      } catch (error){
+        // eslint-disable-next-line
+        console.error(error)
+      }
+    },
+    showMovie(movie) {
+      console.log("I Got here", movie);
+      this.selectedMovie = movie
+      // eslint-disable-next-line
+      console.log(this.movie);
+    }
   }
 }
 </script>
---
+++ src/components/fullMovieList.vue
@@ -0,0 +1,69 @@
+<template>
+
+  <div id="fullMovieList" class="small-container">
+    <h1>All Movies</h1>
+    <div class="titleCards" :style="gridStyle">
+      <button v-for="movie in allMovies" @click="$emit('show:movie', movie)" :key="movie.id" class="movieCard">
+        {{ movie.title }}
+    </button>
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'full-movie-list',
+  data() {
+    return {
+      numberOfColumns: 4,
+    }
+  },
+  props: {
+    allMovies: Array
+  },
+  computed: {
+    gridStyle() {
+      return {
+        gridTemplateColumns: `repeat(${this.numberOfColumns}, minmax(100px, 1fr))`
+      }
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+  .titleCards {
+    display: grid;
+    grid-gap: 1em;
+  }
+
+  .movieCard {
+    margin: 1rem;
+    padding: 1.5rem 2.5rem;
+    font-size: 1.5em;
+    font-weight: 245;
+    background-image: linear-gradient(#ff9966 0%, #ffffcc 100%);
+    margin: 0 0 2rem 0;
+    box-shadow: 0px 0px 15px 10px  #b3c6ff;
+    border-radius: 12px;
+    transition: transform .8s;
+  }
+
+  .movieCard:hover {
+    box-shadow: 0px 0px 85px 25px 20px #b3c6ff;
+    transform: scale(1.5)
+  }
+
+  .small-container {
+    max-width: 1500px;
+    margin: 0 auto;
+
+  }
+
+  h1 {
+      text-align: center;
+    }
+
+</style>
